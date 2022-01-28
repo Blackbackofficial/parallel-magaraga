9287c3c0351ab48c86badb83a75585bb540b4540
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2022-01-28 17:02:05 +0300

    Add flags

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "maharaja.h"
+#include <cstdlib>
 
-int main() {
-    Maharaja m = Maharaja(4,4,5);
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "No arguments" << std::endl;
+        return 0;
+    }
+
+    Maharaja m = Maharaja(atoi(argv[1]),atoi(argv[1]),atoi(argv[2]));
     int positions = m.greedy_filling();
     std::cout << "Всего " << positions << " позиции" << std::endl;
     for (auto pos : m.get_results())
