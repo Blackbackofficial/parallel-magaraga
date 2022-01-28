f5a377c1633034480323efd6e2f264e58b43bf92
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2022-01-28 16:25:57 +0300

    Add parallel

--- main.cpp
+++ main.cpp
@@ -1,29 +1,11 @@
-﻿
 #include <iostream>
-#include <fstream>
-
-#include "chessboard.h"
-#include "log_duration.h"
-
-using namespace std;
-
+#include "maharaja.h"
 
 int main() {
-    size_t chessboard_size = 0;
-    cout << "Введите размер доски: "sv;
-    cin >> chessboard_size;
-
-    while (chessboard_size <= 0) {
-        cout << "Think better, Введите размер доски: "sv;
-        cin >> chessboard_size;
-    }
-
-    Chessboard chessboard(chessboard_size);
-
-    // TODO перенести вывод времени в текстовый файл так же как и колличество процессов
-    LogDuration log("Hard with par");
-    chessboard.PrintHardDecision();
-
+    Maharaja m = Maharaja(4,4,5);
+    int positions = m.greedy_filling();
+    std::cout << "Всего " << positions << " позиции" << std::endl;
+    for (auto pos : m.get_results())
+        std::cout << "(" << pos.first << "," << pos.second << ") ";
     return 0;
 }
-
